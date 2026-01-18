7244a05e4193af89073b8a7fa4d8afdb08225007
Yara Alrasheed <11982@holbertonstudents.com>
2026-01-18 17:23:48 +0300

    Refactor Task struct and add data file constant

--- todo-c/src/todo.h
+++ todo-c/src/todo.h
@@ -1,15 +1,23 @@
-#define TODO_H
 #ifndef TODO_H
+#define TODO_H
 
 #define MAX_TASKS 200
-#define MAX_TEXT 200
+#define MAX_TEXT  200
+#define DATA_FILE "tasks.txt"
 
-type struct {
-  int done
-  
-  } Task;
+typedef struct {
+    int done;
+    char text[MAX_TEXT];
+} Task;
 
-void input_list()
+/* core functions */
+int load_tasks(Task tasks[], int capacity);
+void save_tasks(const Task tasks[], int count);
 
+/* task actions */
+void list_tasks(const Task tasks[], int count);
+void add_task(Task tasks[], int *count);
+void toggle_done(Task tasks[], int count);
+void delete_task(Task tasks[], int *count);
 
 #endif
